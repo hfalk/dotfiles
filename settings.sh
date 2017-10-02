159d3c318741bc946e7c0879e16a5c57331ce20e
Håvard Falk <hfalk@users.noreply.github.com>
2017-10-02 21:13:40 +0200

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -6,8 +6,8 @@ GIT_EMAIL="hfalk4@gmail.com"
 # The applications below are regular apps such as Chrome and Spotify.
 # Please make sure that you remove all the programs you don't want to install on your computer
 # P.S. The installer script won't install the applications unless you tell it to. It's your call  :)
-BREW_CASKS="alfred android-studio appcleaner atom basictex calibre flux gpgtools
-  google-chrome google-drive intellij-idea iterm2 jetbrains-toolbox postman
+BREW_CASKS="alfred android-studio appcleaner atom google-backup-and-sync basictex 
+  calibre flux gpgtools google-chrome intellij-idea iterm2 jetbrains-toolbox postman
   pycharm r-app skype skype-for-business slack spectacle spotify torbrowser
   transmission veracrypt vlc"
 
