c2bb85401594f55e75eb6caec5d0d6752f85a237
Håvard Falk <havard.falk@bekk.no>
2022-04-05 21:57:16 +0200

    Add old dotfiles

--- settings.sh
+++ settings.sh
@@ -6,20 +6,13 @@ GIT_EMAIL="hfalk4@gmail.com"
 # The applications below are regular apps such as Chrome and Spotify.
 # Please make sure that you remove all the programs you don't want to install on your computer
 # P.S. The installer script won't install the applications unless you tell it to. It's your call  :)
-BREW_CASKS="alfred android-studio appcleaner atom google-backup-and-sync basictex
-  calibre flux google-chrome intellij-idea iterm2 java jetbrains-toolbox postman
-  pycharm r-app skype skype-for-business slack spectacle spotify torbrowser
+BREW_CASKS="alfred android-studio appcleaner google-backup-and-sync basictex
+  calibre flux iterm2 java postman
+  pycharm r-app skype skype-for-business spectacle spotify torbrowser
   transmission veracrypt vlc"
 
 # NODE
 NPM_PACKAGES="n ncu"
-
-# ATOM
-APM_PACKAGES="atom-beautify autoclose-html autocomplete-paths busy-signal color-picker
-  editorconfig elm-format elmjutsu expose file-icons git-blame git-time-machine
-  highlight-selected intentions language-babel language-elm language-r linter
-  linter-elm-make linter-eslint linter-flake8 linter-ui-default minimap open-recent
-  pigments seti-syntax seti-ui todo-show"
 
 # BREW (OPTIONAL)
 OPTIONAL_BREW_PACKAGES="bazel gnupg nmap nvm pyenv wget yarn"
@@ -30,14 +23,14 @@ OPTIONAL_BREW_PACKAGES="bazel gnupg nmap nvm pyenv wget yarn"
 # --------------------------------------------------------------
 
 # BREW (ESSENTIAL)
-ESSENTIAL_BREW_PACKAGES="coreutils diff-so-fancy fish git grep mongodb node
+ESSENTIAL_BREW_PACKAGES="coreutils diff-so-fancy git grep mongodb node
   postgresql python python@2 openssh screen vim"
 
-# FISH (installed with fisherman)
-FISHERMAN_PACKAGES="z jbonjean/re-search"
+# ZSH (installed with brew)
+ZSH_BREW_PACKAGES="zsh zsh-autosuggestions zsh-syntax-highlighting"
 
 # BASH (installed with brew)
 BASH_BREW_PACKAGES="bash bash-completion z"
 
 # PYTHON
-PIP_PACKAGES="autopep8 flake8 pygments virtualenv virtualfish"
+PIP_PACKAGES="autopep8 flake8 pygments virtualenv"
---
+++ setup/zsh.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# SCRIPT_DIR == Same folder as the script is placed in
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${SCRIPT_DIR}/../settings.sh"
+source "${SCRIPT_DIR}/utils.sh"
+
+install_zsh_packages() {
+  print_info "Installing brew packages needed by zsh:"
+  print_list $@
+  brew install "$@"
+  print_result $? "Install zsh packages\n"
+}
+
+create_symlinks() {
+  print_info "Creating symlinks to the zsh dotfiles"
+  ${SCRIPT_DIR}/symlink_dotfiles.sh "../dotfiles/zsh/"
+  ${SCRIPT_DIR}/symlink_dotfiles.sh "../dotfiles/universal/"
+  printf "\n"
+}
+
+add_zsh_as_allowed_shell() {
+  # Add zsh to /etc/shells (the list of allowed shells)
+  # If "/usr/local/bin/zsh" isn't already in "/etc/shells", add it to the list"
+  grep -q -f $(which zsh) /etc/shells || echo $(which zsh) | sudo tee -a /etc/shells
+}
+
+install_oh_my_zsh() {
+  if ask_question "Do you want to install Oh My Zsh?"; then
+    rm ~/.config/fish/functions/fish_prompt.fish
+    if test -d $HOME/.oh-my-zsh; then
+      print_error "Oh! Seems like Oh My Zsh were already installed ¯\_(ツ)_/¯\n"
+    else
+      sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+      print_result $? "Install Oh My Zsh\n"
+    fi
+  else
+    print_error "Alright, your loss!"
+  fi
+}
+
+set_zsh_as_default_shell() {
+  if ask_question "Do you want to set Zsh as your default shell?"; then
+    # Set zsh as default shell
+    chsh -s $(which zsh)
+    print_result $? "Set Zsh as default shell\n"
+  else
+    print_error "Alright. When the installer is finished, you can type 'zsh' in the terminal to test it without setting it as your default"
+  fi
+}
+
+print_heading "Zsh"
+install_bash_packages $ZSH_BREW_PACKAGES
+create_symlinks
+add_zsh_as_allowed_shell
+install_oh_my_zsh
+set_zsh_as_default_shell
